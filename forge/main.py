4d75de9744702fbb74c94d7888f512b29b8c3ca6
Marco Rosa <marco1rosa@gmail.com>
2024-04-19 16:21:48 +0200

    Create the configuration file if it's not present in the folder where forge is running

--- forge/main.py
+++ forge/main.py
@@ -2,13 +2,48 @@
 """main file of the Forge CLI"""
 
 import json
+import os
+
+
+def get_config_path():
+    """
+    Calculate the real path of the config file as the symbolic link does not know where to find it
+    """
+    # Original path
+    script_path = os.path.realpath(__file__)
+    # Calculate the path
+    script_dir = os.path.dirname(script_path)
+    config_path = os.path.join(script_dir, "default_config.json")
+
+    return config_path
 
 
 def read_config():
     """Read the configuration file .forge.json."""
-    with open(".forge.json", mode="r", encoding="utf-8") as file:
-        config = json.load(file)
-    return config
+    config_path = ".forge.json"
+
+    try:
+        with open(config_path, mode="r", encoding="utf-8") as file:
+            config = json.load(file)
+        return config
+    except FileNotFoundError:
+        response = input(
+            "Seems that you don't have the configuration file. Do you want to create it? (y/n): "
+        )
+        if response.lower() == "y":
+            # internal configuration file path
+            internal_config_path = get_config_path()
+            print(internal_config_path)
+            # reading the internal configuration file
+            with open(internal_config_path, mode="r", encoding="utf-8") as default_file:
+                default_config = json.load(default_file)
+
+            with open(config_path, mode="w", encoding="utf-8") as new_file:
+                json.dump(default_config, new_file, indent=4)
+
+            print("File .forge.json successfully created")
+
+        return None
 
 
 def main():
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,25 +2,51 @@
 Tests the Forge CLI
 """
 
-import json
+from unittest import mock
+import pytest
+from forge.main import read_config, get_config_path
 
-from unittest.mock import mock_open, patch
-from forge.main import read_config
+
+def test_get_config_path():
+    """Test the get_config_path function to ensure it returns the correct path."""
+    with mock.patch("os.path.realpath", return_value="/path/to/script.json"):
+        with mock.patch("os.path.dirname", return_value="/path/to"):
+            config_path = get_config_path()
+            assert config_path == "/path/to/default_config.json"
 
 
-def test_read_config_success():
-    """Tests the reading the configuration file"""
+def test_read_config_file_exists():
+    """Test read_config when the .forge.json file already exists."""
+    with mock.patch("builtins.open", mock.mock_open(read_data='{"key": "value"}')):
+        with mock.patch("json.load", return_value={"key": "value"}):
+            assert read_config() == {"key": "value"}
 
-    mock_data = {
-        "actions": [
-            {"name": "Build project"},
-            {"name": "Run tests"},
-            {"name": "Clean project"},
-        ]
-    }
-    with patch("builtins.open", mock_open(read_data=json.dumps(mock_data))):
-        with patch("json.load", return_value=mock_data):
-            config = read_config()
 
-            assert config == mock_data
-            assert config["actions"][1]["name"] == "Run tests"
+def test_read_config_file_not_exists_user_creates():
+    """Test read_config when .forge.json does not exist and the user chooses to create it."""
+    with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+        mocked_file.side_effect = [FileNotFoundError(), mock.DEFAULT, mock.DEFAULT]
+        with mock.patch("json.load", return_value={"default_key": "default_value"}):
+            with mock.patch("builtins.input", return_value="y"):
+                with mock.patch(
+                    "forge.main.get_config_path",
+                    return_value="/path/to/default_config.json",
+                ):
+                    assert read_config() is None
+                    mocked_file.assert_called_with(
+                        ".forge.json", mode="w", encoding="utf-8"
+                    )
+
+
+def test_read_config_file_not_exists_user_declines():
+    """Test read_config when .forge.json does not exist and the user chooses not to create it."""
+    with mock.patch("builtins.open", mock.mock_open()) as mocked_file:
+        mocked_file.side_effect = FileNotFoundError()
+        with mock.patch("builtins.input", return_value="n"):
+            assert read_config() is None
+            mocked_file.assert_called_with(".forge.json", mode="r", encoding="utf-8")
+
+
+# Run the tests
+if __name__ == "__main__":
+    pytest.main()
